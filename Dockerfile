b326cdc8fa9efc2983cd153f2522701e8abbe484
GambetaClub <marigyt.maj@gmail.com>
2025-05-12 17:29:27 -0300

    Refactor Docker setup and enhance Prisma integration
    - Updated Dockerfile to use Node.js 20.11.1 and streamline the build process.
    - Added Redis service to docker-compose for caching.
    - Modified cities module to use async configuration for Redis connection.
    - Introduced new Prisma commands in package.json for better database management.
    - Updated schema.prisma with binary targets for improved compatibility.

--- Dockerfile
+++ Dockerfile
@@ -1,41 +1,24 @@
-# Use an official Node.js runtime as a parent image
-FROM node:18-alpine AS development
+FROM node:20.11.1-alpine
 
-# Set the working directory
-WORKDIR /usr/src/app
+WORKDIR /app
 
-# Copy package.json and pnpm-lock.yaml
-COPY package.json pnpm-lock.yaml ./
-
-# Install pnpm
 RUN npm install -g pnpm
 
-# Install app dependencies
+COPY package.json ./
+COPY pnpm-lock.yaml ./
+
 RUN pnpm install --frozen-lockfile
 
-# Copy app source
 COPY . .
 
-# Build the app
+COPY src/database/schema.prisma ./src/database/schema.prisma
+
+RUN npx prisma generate --schema ./src/database/schema.prisma
+
 RUN pnpm run build
 
-# Production image
-FROM node:18-alpine AS production
+RUN pnpm prune --prod
 
-ARG NODE_ENV=production
-ENV NODE_ENV=${NODE_ENV}
-
-WORKDIR /usr/src/app
-
-COPY package.json pnpm-lock.yaml ./
-RUN npm install -g pnpm
-RUN pnpm install --prod --frozen-lockfile
-
-COPY --from=development /usr/src/app/dist ./dist
-COPY --from=development /usr/src/app/node_modules ./node_modules
-
-# Expose port 3000
 EXPOSE 3000
 
-# Command to run the application
-CMD ["node", "dist/main.js"] 
+CMD ["pnpm", "start:migrate:prod"] 
--- src/cities/cities.module.ts
+++ src/cities/cities.module.ts
@@ -3,7 +3,7 @@ import { CitiesController } from './cities.controller';
 import { CitiesService } from './cities.service';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from 'src/database/prisma.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { WeatherModule } from 'src/weather/weather.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-redis-store';
@@ -11,14 +11,22 @@ import * as redisStore from 'cache-manager-redis-store';
 const ttl = 60 * 60 * 1000; // 1 hour
 
 @Module({
-  imports: [HttpModule, PrismaModule, ConfigModule, WeatherModule, 
-    CacheModule.register({
-      store: redisStore,
-      socket: {
-        host: 'localhost',
-        port: 6379,
-      },
-      ttl: ttl,
+  imports: [
+    HttpModule,
+    PrismaModule,
+    ConfigModule,
+    WeatherModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        socket: {
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+        },
+        ttl: ttl,
+      }),
     }),
   ],
   controllers: [CitiesController],
